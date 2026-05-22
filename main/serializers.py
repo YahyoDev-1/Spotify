fe66486d53d09f2f593627faacf5ad6b77d365a8
Abdujabborov Yahyobek <abdujabborovyahyobek44@gmail.com>
2026-05-22 08:51:57 +0500

    Add music validate function

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,10 @@
+import datetime
+
 from rest_framework import serializers
 
 from .models import Singer, Album, Song
+
+from mutagen import File as MusicFile
 
 
 class SingerSerializer(serializers.ModelSerializer):
@@ -22,21 +26,43 @@ class AlbumSerializer(serializers.ModelSerializer):
 
 
 class SongSerializer(serializers.ModelSerializer):
-    album = serializers.StringRelatedField(
-        read_only=True,
+    album = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Album.objects.all(),
     )
+
     class Meta:
         model = Song
-        fields = '__all__'
+        fields = ('id', 'name', 'genre', 'file', 'album', 'duration',)
+    #     Duration avtomatik hisoblanadi, foydalanuvchi kiritmaydi
+        read_only_fields = ('duration',)
 
-    def validate_duration(self, duration):
-        # 'duration' bu yerda allaqachon timedelta obyekti hisoblanadi.
-        # total_seconds() umumiy soniyalarni qaytaradi (masalan, 6:00 -> 360.0 soniya).
-        # 7 minutimiz esa: 7 * 60 = 420 soniya bo'ladi.
+    def validate(self, data):
+        file = data.get('file')
 
-        if duration and duration.total_seconds() > 7 * 60:
-            raise serializers.ValidationError(
-                'Qo\'shiq davomiyligi 7 minutdan ko\'p bo\'lmasligi kerak.'
-            )
+        if file:
+            try:
+                # Fayl ko'rsatkichini boshiga qaytaramiz (xavfsizlik uchun)
+                file.seek(0)
 
-        return duration
+                audio = MusicFile(file)
+
+                if audio is not None and audio.info:
+
+                    seconds = audio.info.length
+
+                    if seconds > 7 * 60:
+                        raise serializers.ValidationError(
+                            {"audio_file": "Qo'shiq davomiyligi 7 minutdan ko'p bo'lmasligi kerak."}
+                        )
+                    data['duration'] = datetime.timedelta(seconds=int(seconds))
+                else:
+                    raise serializers.ValidationError(
+                        {"audio_file": "Yuklangan fayl yaroqli audio fayl emas yoki formati qo'llab-quvvatlanmaydi."}
+                    )
+
+            except Exception as e:
+                raise serializers.ValidationError(
+                    {"audio_file": f"Audio faylni tahlil qilishda xatolik yuz berdi: {str(e)}"}
+                )
+        return data
